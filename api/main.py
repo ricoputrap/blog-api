d39e56a019ad58596d4177b4092661fbcd678b23
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-08-31 22:05:57 +0700

    Initiate API project

---
+++ api/main.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from api.utils import db, migrate
+
+def init_app():
+  app = Flask(__name__)
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+  
+  db.init_app(app)
+  migrate.init_app(app, db)
+
+  return app
