c31e3c1a687f7638e1ecb5233c93ce4de568ae9a
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-09-01 20:29:43 +0700

    Update db models: post and user

--- api/models/post.py
+++ api/models/post.py
@@ -1,14 +1,14 @@
 from api.utils import db
 
 class PostModel(db.Model):
-  __tablename__ = 'Post'
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(50), nullable=False)
-  subtitle = db.Column(db.String(255), nullable=True)
-  content = db.Column(db.Text, nullable=False)
-  cover_picture = db.Column(db.Text, nullable=True)
-  creator = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
-  status = db.Column(db.String(4), nullable=False, default='DRFT')
-  created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
-  updated_at = db.Column(db.DateTime, nullable=True)
-  published_at = db.Column(db.DateTime, nullable=True)
+  __tablename__ = 'post'
+  p_id = db.Column(db.String(50), primary_key=True)
+  p_title = db.Column(db.String(50), nullable=False)
+  p_subtitle = db.Column(db.String(255), nullable=True)
+  p_content = db.Column(db.Text, nullable=False)
+  p_cover = db.Column(db.Text, nullable=True)
+  p_creator = db.Column(db.String(50), db.ForeignKey('user.u_id'), nullable=False)
+  p_status = db.Column(db.String(4), nullable=False, default='DRFT')
+  p_created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+  p_updated_at = db.Column(db.DateTime, nullable=True)
+  p_published_at = db.Column(db.DateTime, nullable=True)
--- api/models/user.py
+++ api/models/user.py
@@ -1,11 +1,10 @@
 from api.utils import db
 
 class UserModel(db.Model):
-  __tablename__ = 'User'
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.String(50), nullable=False, unique=True)
-  username = db.Column(db.String(50), nullable=False, unique=True)
-  password = db.Column(db.String(80), nullable=False)
-  email = db.Column(db.String(255), nullable=False, unique=True)
-  fullname = db.Column(db.String(255), nullable=False)
-  occupation = db.Column(db.String(255), nullable=True)
+  __tablename__ = 'user'
+  u_id = db.Column(db.String(50), primary_key=True)
+  u_username = db.Column(db.String(50), nullable=False, unique=True)
+  u_password = db.Column(db.String(80), nullable=False)
+  u_email = db.Column(db.String(255), nullable=False, unique=True)
+  u_fullname = db.Column(db.String(255), nullable=False)
+  u_occupation = db.Column(db.String(255), nullable=True)
