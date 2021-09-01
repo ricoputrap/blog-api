c7ec1a3f7fae79f45c22b1e56ab972590ab41691
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-09-01 21:02:40 +0700

    Implement GET user by u_id

--- api/controllers/v1/user.py
+++ api/controllers/v1/user.py
@@ -1,3 +1,4 @@
+from flask import make_response, jsonify
 from flask_restful import Resource
 from api.services.user import UserService
 from api.serializers.user import UserSchema
@@ -16,8 +17,25 @@ class User(Resource):
           'data': self.user_list_schema.dump(users),
           'total': len(users)
         }
-        return response
       else:
-        pass
+        user = self.user_service.get_user_by_id(u_id)
+
+        if not user:
+          return make_response(jsonify({
+            'errors': {
+              'status': 404,
+              'title': 'User not found',
+              'detail': 'User with u_id = %s is not found' % (u_id),
+              'source': {
+                'pointer': '/profile/<u_id>',
+                'method': 'GET'
+              },
+            }
+          }), 404)
+        
+        response = {
+          'data': self.user_schema.dump(user)
+        }
+      return response
     except Exception as e:
       pass
--- api/services/user.py
+++ api/services/user.py
@@ -5,3 +5,7 @@ class UserService:
   def get_all_users(self):
     users = UserModel.query.all()
     return users
+  
+  def get_user_by_id(self, u_id):
+    user = UserModel.query.filter_by(u_id=u_id).first()
+    return user
